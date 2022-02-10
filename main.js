d790847a09317ecbef3a65108229d1807efccc85
SampurnaGope <85182200+SampurnaGope@users.noreply.github.com>
2022-02-10 10:27:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+   
+    lips = loadImage('https://i.postimg.cc/prXvMH3D/lips.png');
+
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+  
+    image(lips,  noseX-20, noseY+10,  40, 30);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Intialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+
+       
+
+
+    }
+}
